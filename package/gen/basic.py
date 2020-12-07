ba6870bc0c4b73dea5a5e4a778caaea8edc45d64
silentunion <milliner@unbc.ca>
2020-12-07 07:48:37 -0800

    Created single word generation based on template

--- package/gen/basic.py
+++ package/gen/basic.py
@@ -3,4 +3,23 @@ import random
 from ..api import db_request
 
 letters = db_request.get_letters()
-print(letters)
+
+vowels, consonants = [], []
+
+for row in letters:
+    if row['is_vowel']:
+        vowels.append(row['letter'])
+    else:
+        consonants.append(row['letter'])
+
+template = 'cvcvcv'
+word = ''
+
+for letter in range(0, len(template)):
+    let = template[letter]
+    if let == 'v':
+        word += random.choice(vowels)
+    else:
+        word += random.choice(consonants)
+
+print(word)
