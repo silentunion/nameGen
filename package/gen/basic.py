8d923ef730938b1addcd559b2419062b6cb9de17
silentunion <milliner@unbc.ca>
2020-12-10 07:18:12 -0800

    Added completely random generation endpoint

--- package/gen/basic.py
+++ package/gen/basic.py
@@ -2,7 +2,7 @@ import random
 
 from ..api import db_request
 
-def generate(template, num):
+def get_letters_from_db():
     letters = db_request.get_letters()
 
     vowels, consonants = [], []
@@ -13,12 +13,42 @@ def generate(template, num):
         else:
             consonants.append(row['letter'])
 
+    return vowels, consonants
+
+
+def generate(template, num_words):
+    vowels, consonants = get_letters_from_db()
+
     template = template
     words = []
-    num_words = num
 
-    for n in range(num_words):
+    for num in range(num_words):
         word = ''
+        for letter in range(0, len(template)):
+            let = template[letter]
+            if let == 'v':
+                word += random.choice(vowels)
+            else:
+                word += random.choice(consonants)
+        words.append({'word': word})
+    
+    return words
+
+
+def completely_random(num_words):
+    vowels, consonants = get_letters_from_db()
+
+    choices = ['v', 'c']
+    words = []
+
+    for num in range(num_words):
+        template = ''
+        word = ''
+
+        word_length = random.randint(2, 12)
+        for l in range(0, word_length):
+            template += random.choice(choices)
+
         for letter in range(0, len(template)):
             let = template[letter]
             if let == 'v':
