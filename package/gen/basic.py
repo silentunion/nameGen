2c5e391f9788162a226e465e1be63c16ba3ec87a
silentunion <milliner@unbc.ca>
2020-12-28 10:44:13 -0800

    Removed older unused code

--- package/gen/basic.py
+++ package/gen/basic.py
@@ -8,58 +8,6 @@ sr.get_clusters_from_db
 vowels, consonants, v_freq, c_freq = sr.get_letters_from_db()
 cl_vowels, cl_consonants = sr.get_clusters_from_db()
 
-def generate(**kwargs):
-    names = []
-    template = kwargs['template']
-    num_names = kwargs['num']
-    is_weighted = kwargs['is_weighted']
-    is_random = kwargs['is_random']
-    min_letters = kwargs['min_letters'] if 'min_letters' in kwargs else 2
-    max_letters = kwargs['max_letters'] if 'max_letters' in kwargs else 12
-
-    print
-
-    literal = ''
-    if len(template) > 0 and ('(' or ')') in template:
-        start = '('
-        end = ')'
-        literal = template[template.find(start) + len(start):template.rfind(end)]
-
-    print('literal: ' , literal)
-
-    for num in range(num_names):
-        if is_random:
-            template = ''
-            choices = ['v', 'c']
-            name_length = random.randint(min_letters, max_letters)
-            is_double = False
-            for l in range(0, name_length):
-                letter_type = random.choice(choices)
-                while is_double:
-                    if letter_type == template[-1]:
-                        letter_type = random.choice(choices)
-                    else:
-                        is_double = False
-                if len(template) > 0 and letter_type == template[-1]:
-                    is_double = True
-                template += letter_type
-            
-        name = ''
-        for letter in range(0, len(template)):
-            let = template[letter]
-            if is_weighted:
-                if let == 'v':
-                    name += random.choices(vowels, weights=v_freq, k=1)[0]          
-                else:
-                    name += random.choices(consonants, weights=c_freq, k=1)[0]
-            else:
-                if let == 'v':
-                    name += random.choice(vowels)
-                else:
-                    name += random.choice(consonants)
-        names.append({'name': name.capitalize()})
-    
-    return names
 
 def generate_random(**kwargs):    
     num_names = kwargs['num']
